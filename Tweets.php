e246ea306cf166f5c8e6b13314f9cf2e08f23c4a
Chanaka Lakmal <ldclakmal@gmail.com>
2017-04-26 17:55:43 +0530

    Initial commit

---
+++ Tweets.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="www.avix.lk"/>
+        <meta name="author" content="L.D. Chanaka Lakmal"/> 
+        <title>EXMO | CSE | Tweets</title>
+        <link rel="shortcut icon" href="img/favicon.png">
+    </head>
+    <body style="background-image: url('img/background.jpg'); text-align: center;">
+        <div class="col-xs-12">
+            <div class="row">
+                <img src="img/logo.png" alt="logo" class="center-block" id="logo" style="width: 350px;">
+                <br/>
+            </div>
+        </div>
+        <div>
+            <a class="twitter-timeline" href="https://twitter.com/hashtag/nightofthewizards" data-widget-id="644039617633648644">#nightofthewizards Tweets</a>
+            <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + "://platform.twitter.com/widgets.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, "script", "twitter-wjs");</script>
+            <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + "://platform.twitter.com/widgets.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, "script", "twitter-wjs");</script>
+        </div>
+    </body>
+</html>
